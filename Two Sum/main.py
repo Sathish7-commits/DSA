779cab6fc356957ee00333c982192db51f7037db
Sathish7-commits <59795182+Sathish7-commits@users.noreply.github.com>
2024-08-24 09:34:46 +0530

    Create main.py

---
+++ Two Sum/main.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        complements = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+
+            if complement in complements:
+                return [i, complements[complement]]
+            
+            complements[nums[i]] = i
