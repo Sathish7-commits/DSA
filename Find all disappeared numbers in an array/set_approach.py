c68d9236504d09e4f66df73edbfb5ed463f6fc1c
Sathish7-commits <59795182+Sathish7-commits@users.noreply.github.com>
2024-11-02 08:14:09 +0530

    Update set_approach.py

--- Find all disappeared numbers in an array/set_approach.py
+++ Find all disappeared numbers in an array/set_approach.py
@@ -1,4 +1,16 @@
 class Solution:
+    """
+    Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+    
+    Example 1:
+        Input: nums = [4,3,2,7,8,2,3,1]
+        Output: [5,6]
+    
+    Example 2:
+        Input: nums = [1,1]
+        Output: [2]
+    """
+    
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         s = set(nums)
         return [i for i in range(1, len(nums)+1) if i not in s]
