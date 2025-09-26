47c79b154ac8c27a896e5d59695cc97844a36a56
Sathish7-commits <sathishofficial7@gmail.com>
2025-09-26 12:23:21 +0530

    trap rain water

--- Two Pointer/18. Trapping Rain Water/trapping_rain_water.py
+++ Two Pointer/18. Trapping Rain Water/trapping_rain_water.py
@@ -5,6 +5,7 @@ class Solution:
     You are given an array of non-negative integers height which represent an elevation map. Each value height[i] represents the height of a bar, which has a width of 1.
 
     Return the maximum area of water that can be trapped between the bars.
+    https://www.youtube.com/watch?v=KFdHpOlz8hs
     """
     def trap(self, height: List[int]) -> int:
         n = len(height)
