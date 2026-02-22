ad6a0522d2fe3491cde688b0af20ad99eaaaf5c6
Sathish7-commits <sathishofficial7@gmail.com>
2026-02-23 01:36:00 +0530

    sub array equals k

--- Leetcode/560.subarray-sum-equals-k.py
+++ Leetcode/560.subarray-sum-equals-k.py
@@ -30,6 +30,48 @@ class Solution:
         -107 <= k <= 107
     '''
     def subarraySum(self, nums: List[int], k: int) -> int:
+        # prefix sum method maintains a running total and subtracting from k shows us if the element is present in the array
+        '''
+        # nums = [7,4,3,2,1,3,5,6], k = 1
+        i=0 → 7
+
+        prefix_sum = 7
+        Need 7-1 = 6 → not in freq
+        freq = {0:1, 7:1}
+
+        i=1 → 4
+
+        prefix_sum = 11
+        Need 11-1 = 10 → not in freq
+        freq = {0:1,7:1,11:1}
+
+        i=2 → 3
+
+        prefix_sum = 14
+        Need 14-1 = 13 → not found
+        freq add 14
+
+        i=3 → 2
+
+        prefix_sum = 16
+        Need 16-1 = 15 → not found
+        freq add 16
+
+        i=4 → 1
+
+        prefix_sum = 17
+        Need 17-1 = 16
+
+        Is 16 in freq?
+        Yes.
+
+        So:
+
+        count += freq[16]
+        freq[16] = 1
+
+        count = 1
+        '''
         prefix_sum = 0
         count = 0
         frequency = {0:1}
@@ -37,7 +79,7 @@ class Solution:
         for i in range(len(nums)):
             prefix_sum += nums[i]
 
-            if prefix_sum - k in frequency:
+            if prefix_sum - k in frequency: # prefix_sum - k -> actually helps checks the sum between subarray
                 count += frequency[prefix_sum-k]
 
             frequency[prefix_sum] = frequency.get(prefix_sum,0)+1
